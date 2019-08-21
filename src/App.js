9e4b25b10d620ac6c833cb93871d11b308f429b9
HieuNguyen <minhhieu1896@gmail.com>
2019-08-21 17:10:37 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import {HomePage} from './homepage.component.jsx';
+
+class App extends Component {
+  render() {
+    return <div className='App'>
+      <HomePage/>
+    </div>;
+  }
+}
+
+export default App;
