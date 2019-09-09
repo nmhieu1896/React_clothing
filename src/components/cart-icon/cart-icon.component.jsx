34ec7204e8068d47cf64d96be2b2952580839b93
HieuNguyen <minhhieu1896@gmail.com>
2019-09-09 16:21:40 +0700

    update cart-dropdown and cart cart-icon:number when click add-to-cart on shop product

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,19 +6,21 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, cartItems}) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className="item-count">0</span>
+        <span className="item-count">{
+            cartItems.reduce((quantity, item) => quantity += item.quantity, 0)
+        }</span>
     </div>
 );
 
-// const mapStateToProps = ({cart}) => {
-    
-// }
+const mapStateToProps = ({cart}) => ({
+    cartItems: cart.cartItems
+})
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
