e50f187b4acefe22578008fa32620d1017ae6612
HieuNguyen <minhhieu1896@gmail.com>
2019-09-09 15:40:54 +0700

    update cart with adding item and check if existing or not

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { connect } from 'react-redux';
+import CustomButton from '../custom-button/custom-button.component';
+
+import { addItem } from '../../redux/cart/cart.action';
+
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ id, name, imageUrl, price }) => (
+const CollectionItem = ({ item, addItem }) => {
+  const { id, name, price, imageUrl } = item;
+  return (
   <div className="collection-item">
     <div
       className="image"
@@ -14,7 +21,14 @@ const CollectionItem = ({ id, name, imageUrl, price }) => (
       <span className="name">{name}</span>
       <span className="price">{`$${price}`}</span>
     </div>
-  </div>
-);
 
-export default CollectionItem;
+    <CustomButton inverted onClick={() => addItem(item)} >Add to cart</CustomButton>
+  </div>
+  )
+};
+
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -9,8 +9,8 @@ const PreviewCollection = ( {title, items} ) => (
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className="preview">
             {
-                items.filter((el, index) => index < 4).map( ({id, ...otherProps}) => (
-                    <CollectionItem key={id} {...otherProps} />
+                items.filter((el, index) => index < 4).map( item => (
+                    <CollectionItem key={item.id} item={item} />
                 ) )
             }
         </div>
