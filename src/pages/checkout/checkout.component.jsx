5e02bf3bdc8f72fe5afa6e64e63784c7aeec2fdc
nmhieu1896 <nmhieu1896@gmail.com>
2019-09-17 23:39:29 +0700

    adding stripe checkout API

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import { createStructuredSelector } from 'reselect';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selector';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
  
 import './checkout.styles.scss';
 
@@ -34,6 +35,11 @@ const CheckoutPage = ({ cartItems, total }) => (
         <div className="total">
             <span>TOTAL: ${total}</span>
         </div>
+        <div className="test-warning">
+            *Please use following test credit card for payments* <br/>
+            4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
@@ -43,3 +49,5 @@ const mapStateToProps = createStructuredSelector({
 });
 
 export default connect(mapStateToProps)(CheckoutPage);
+
+// 4242 4242 4242 4242 - 01/20 - 123
